cec05466a4cbec73289ff17c5c5285e699b7f209
Rafael <r.otavio.tenorio@gmail.com>
2024-04-14 19:09:10 -0300

    InitialCommit

---
+++ TADFila.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoFila
+{
+    internal class TADFila
+    {
+        private class Nodulo
+        {
+            public int Codigo { get; set; }
+            public string? Descricao { get; set; }
+            public int Capacidade { get; set; }
+            public Nodulo Next { get; set; }
+            
+            public Nodulo(int codigo, string descricao, int capacidade)
+            {
+                Codigo = codigo;
+                Descricao = descricao;
+                Capacidade = capacidade;
+                Next = null;
+            }
+        }
+        private Nodulo primeiro;
+        private Nodulo ultimo;
+
+        public TADFila(int codigo)
+        {
+            primeiro = null;
+            ultimo = null;
+            IncluirNaFila(codigo);
+        }
+
+        public int? RetirarDaFila()
+        {
+            if (primeiro == null)
+            {
+                Console.WriteLine("A fila está vazia.");
+                return null;
+            }
+            int codigo = primeiro.Codigo;
+            primeiro = primeiro.Next;
+            if (primeiro == null)
+            {
+                ultimo = null;
+            }
+            return codigo;
+        }
+
+        public void IncluirNaFila(int k)
+        {
+            Console.WriteLine($"Digite a descrição do elemento de código {k}:");
+            string descricao = Console.ReadLine();
+            Console.WriteLine($"Digite a capacidade do elemento de código {k}");
+            int capacidade = int.Parse(Console.ReadLine());
+            Nodulo novoNodulo = new Nodulo(k, descricao, capacidade);
+            if (ultimo == null)
+            {
+                primeiro = novoNodulo;
+                ultimo = novoNodulo;
+            } else {
+                ultimo.Next = novoNodulo;
+                ultimo = novoNodulo;
+            }
+        }
+
+        public void ImprimirFila()
+        {
+            if (primeiro == null)
+            {
+                Console.WriteLine("A fila está vazia.");
+                return;
+            }
+            Console.WriteLine("Conteúdo da fila:");
+            Console.WriteLine($"Primeiro elemento: Código: {primeiro.Codigo}, Descrição: {primeiro.Descricao}, Capacidade: {primeiro.Capacidade}");
+            Console.WriteLine($"Último elemento: Código: {ultimo.Codigo}, Descrição: {ultimo.Descricao}, Capacidade: {ultimo.Capacidade}");
+
+            Nodulo atual = primeiro;
+            while (atual != null)
+            {
+                Console.WriteLine($"Código: {atual.Codigo}, Descrição: {atual.Descricao}, Capacidade: {atual.Capacidade}");
+                atual = atual.Next;
+            }
+
+        }
+    }
+}
---
+++ TestarFila.cs
@@ -0,0 +1,58 @@
+﻿namespace ProjetoFila
+{
+    internal class TestarFila
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nInício main");
+            TADFila fila = new TADFila(1);
+            //fila.IncluirNaFila(5);
+            //fila.IncluirNaFila(11);
+            //fila.IncluirNaFila(3);
+            //fila.ImprimirFila();
+            //fila.IncluirNaFila(20);
+            //fila.IncluirNaFila(5);
+            //fila.ImprimirFila();
+            //Console.WriteLine(fila.RetirarDaFila());
+            //Console.WriteLine(fila.RetirarDaFila());
+            //fila.ImprimirFila();
+
+            do
+            {
+                try
+                {
+                    Console.WriteLine("O que deseja fazer? \n1- Adicionar elemento na fila. \n2- Remover elemento da fila. \n3- Imprimir fila.");
+                    int selecao = int.Parse(Console.ReadLine());
+                    switch (selecao)
+                    {
+                        case 1:
+                            Console.WriteLine("\nDigite o código do elemento a ser adicionado na fila:");
+                            fila.IncluirNaFila(int.Parse(Console.ReadLine()));
+                            break;
+                        case 2:
+                            fila.RetirarDaFila();
+                            Console.WriteLine("\nElemento removido.");
+                            break;
+                        case 3:
+                            fila.ImprimirFila();
+                            break;
+                    }
+                    Console.WriteLine("\nDeseja realizar outra operação? \n1- Sim. \n2- Não.");
+                    selecao = int.Parse(Console.ReadLine());
+                    if (selecao == 2)
+                    {
+                        break;
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("\nErro:");
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine("\n");
+                }
+            } while (true);
+            Console.WriteLine("\nFim main");
+        }
+    }
+}
